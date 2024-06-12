386ac32e2e353dd5ab0dceebb4392967d5a11db0
Ed Page <eopage@gmail.com>
2024-06-12 15:13:24 -0500

    fix: Switch to updated theme

--- extra/config/nvim/lua/plugins/theme.lua
+++ extra/config/nvim/lua/plugins/theme.lua
@@ -1,12 +1,17 @@
 vim.o.background = "light"
+vim.g.material_style = "lighter"
 
 return {
   { "Th3Whit3Wolf/one-nvim" },
+  { 'projekt0n/github-nvim-theme' },
+  { 'marko-cerovac/material.nvim' },
 
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "one-nvim",
+      -- colorscheme = "one-nvim",
+      colorscheme = "github_light_default",
+      -- colorscheme = "material",
     },
   },
 }
